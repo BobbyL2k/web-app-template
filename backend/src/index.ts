359ed8d8a0997477a9abeedded492cb16d22293b
Anuruth Lertpiya <l2k.bobby@gmail.com>
2024-11-03 00:35:11 +0700

    Lint and format backend with biome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import assert from "@/lib/utils/assert.js";
-import { sum } from "./sum";
+import assert from '@/lib/utils/assert.js';
+import { sum } from './sum';
 
 console.log(sum(1, 1));
-assert(true, "assert error");
+assert(true, 'assert error');
